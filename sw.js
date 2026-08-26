5ed0126404fac1cc9fb25ba0306c1213c2ae122e
Danik2829 <maximenko21282908@gmail.com>
2026-08-26 12:01:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,122 @@
-const CACHE_NAME = "bgmu-schedule-v1";
+const CACHE_NAME =
+  "bgmu-schedule-v3";
 
-const APP_FILES = [
+
+const FILES = [
+
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
+
   "./data/schedule.json",
+
   "./icons/icon-180.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
+
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
-      .then(() => self.skipWaiting())
-  );
-});
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
+self.addEventListener(
+  "install",
+  event => {
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+    event.waitUntil(
 
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(FILES)
+        )
 
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      });
-    })
-  );
-});
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys =>
+
+          Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          )
+
+        )
+
+        .then(() =>
+          self.clients.claim()
+        )
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    if (
+      event.request.method !== "GET"
+    ) {
+      return;
+    }
+
+
+    event.respondWith(
+
+      fetch(event.request)
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(cache => {
+
+              cache.put(
+                event.request,
+                copy
+              );
+
+            });
+
+          return response;
+
+        })
+
+        .catch(() =>
+          caches.match(
+            event.request
+          )
+        )
+
+    );
+
+  }
+);
